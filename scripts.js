4f1f4cbb63482dd5f7795700ed9f3dcdb08e9aa8
MagnusLaid <laid.magnus@gmail.com>
2023-03-23 16:47:29 +0200

    Kodutöö counter

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+
+function plus() {
+    count++;
+    document.getElementById("counter").innerHTML = count;
+}
+
+
+function minus() {
+    count--;
+    document.getElementById("counter").innerHTML = count;
+}
+
+
+function reset() {
+    count = 1;
+    document.getElementById("counter").innerHTML = count;
+}
